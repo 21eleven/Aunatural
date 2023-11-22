cb49edb9fe64546675d1180aa25a65dad7b2f036
Noah Lidell <noahlidell@gmail.com>
2023-11-22 14:54:59 -0600

    init natty def

---
+++ Aunatural.lean
@@ -0,0 +1,24 @@
+-- This module serves as the root of the `Aunatural` library.
+-- Import modules here that should be built as part of the library.
+import «Aunatural».Basic
+import Aunatural.NattyLite
+
+#eval hello
+
+example (x : Nat) : x = x := by
+  rfl
+
+example
+  (x y : Nat)
+  (h : y = x + 7) : 2 * y = 2 * (x + 7) := by
+    rw [h]
+
+#eval Nat.succ 1
+
+theorem one_eq_succ_zero : 1 = Nat.succ 0 := by
+  rfl
+
+example : 2 = Nat.succ (Nat.succ 0) := by
+  rfl
+
+
---
+++ TutorialWorld.lean
@@ -0,0 +1,32 @@
+import Aunatural.NattyLite
+
+namespace Natty
+open Natty
+/-
+if we don't set the namespace then in p1 we have:
+  ▶ 5:28-5:33: error:
+  failed to synthesize instance
+    HMul Natty Natty ?m.33
+-/
+
+lemma p1 (x y z : Natty) : x * y + z = x * y + z := by
+  rfl
+
+
+lemma p2 (x y : Natty) (h : y = x + 7): 2 * y = 2 * (x + 7) := by
+  rewrite [h]
+  rfl
+
+lemma p3 : 2 = succ (succ 0) := by
+  rfl
+  /-
+    not sure why it don't say:
+      equality lhs
+        2
+      is not definitionally equal to rhs
+        succ (succ 0)
+      ⊢ 2 = succ (succ 0)
+    like it does on the natural number game website
+  -/
+
+
