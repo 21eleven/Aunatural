afbffe28639b86ed1576529b91dbdac9b56e10c1
Noah Lidell <noahlidell@gmail.com>
2023-11-25 16:13:01 -0600

    NattyLite -> Natty

--- Aunatural.lean
+++ Aunatural.lean
@@ -1,7 +1,7 @@
 -- This module serves as the root of the `Aunatural` library.
 -- Import modules here that should be built as part of the library.
 import «Aunatural».Basic
-import Aunatural.NattyLite
+import Aunatural.Natty
 
 #eval hello
 
